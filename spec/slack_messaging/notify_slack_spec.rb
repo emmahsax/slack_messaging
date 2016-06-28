636a82ea5f140a556a35eb2a7ecc3e049972b11e
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2016-06-28 17:03:30 -0500

    Adding basis of slack messaging

---
+++ spec/slack_messaging/notify_slack_spec.rb
@@ -0,0 +1,12 @@
+require "slack_messaging"
+
+module SlackMessaging
+  describe NotifySlack do
+    
+    it "should ping Slack" do
+        # expect(Aws::SharedCredentials).to receive(:new).with(profile_name: 'staging')
+        # AwsAuditor::NotifySlack.new("test string")
+    end
+    
+  end
+end
