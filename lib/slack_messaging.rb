41994e2f52c99b232e87db709f06bcc3cc2de097
Emma Sax <emma.the.coder@mail.com>
2016-06-29 16:01:12 -0500

    Adding basic tests for RandomMessage

--- lib/slack_messaging.rb
+++ lib/slack_messaging.rb
@@ -1,6 +1,8 @@
 require_relative 'slack_messaging/version'
 require_relative 'slack_messaging/output'
 require_relative 'slack_messaging/config'
+require_relative 'slack_messaging/notify_slack'
+require_relative 'slack_messaging/random_message'
 
 module SlackMessaging
 
