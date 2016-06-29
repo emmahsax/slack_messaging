19f26b91276e5a51007a8a960197d2b22a13bde5
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2016-06-29 16:01:12 -0500

    Adding basic tests for RandomMessage

--- spec/slack_messaging/random_message_spec.rb
+++ spec/slack_messaging/random_message_spec.rb
@@ -3,9 +3,24 @@ require "slack_messaging"
 module SlackMessaging
   describe RandomMessage do
     
-    it "should get a message from an array" do
-        
+    it "should get a string message" do
+      message = RandomMessage.new
+      expect(message.get_text).to be_instance_of(String)
     end
-    
+
+    it "should get a message greater than 25 characters" do
+      message = RandomMessage.new
+      expect(message.get_text.length).to be >= 50
+    end
+
+    it "should get a message that includes a new line" do
+      message = RandomMessage.new
+      expect(message.get_text.include?("\n")).to eq(true)
+    end
+
+    it "should get a message that includes a '--'" do
+      message = RandomMessage.new
+      expect(message.get_text.include?("--")).to eq(true)
+    end
   end
 end
