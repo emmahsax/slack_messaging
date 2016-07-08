f730a0e6bbb777a5760d090d9277b308fc2da7c6
Emma Sax <emma.the.coder@mail.com>
2016-07-08 16:39:57 -0500

    Cleaning up README and config

--- spec/slack_messaging/config_spec.rb
+++ spec/slack_messaging/config_spec.rb
@@ -4,32 +4,13 @@ require 'slack_messaging'
 module SlackMessaging
   describe Config do
 
-    context "#set_config_options" do
-      it "should create a corresponding method that returns the value" do
-        SlackMessaging::Config.set_config_options(test: "something")
-        expect(SlackMessaging::Config.test).to eql("something")
-      end
-
-      it "setting a new key should return the entire config hash" do
-        items = SlackMessaging::Config.config.count
-        expect(SlackMessaging::Config.set_config_options(test1: "something_else").count).to eql(items + 1)
-        expect(SlackMessaging::Config.set_config_options(test2: "something else").count).to eql(items + 2)
-      end
-    end
-
-    context "#config_data" do
-      it "should be a private method" do
-        expect(SlackMessaging::Config).not_to respond_to(:config_data)
-      end
-    end
-
     context "config key methods" do
       it "should return nil when not set" do
         expect(SlackMessaging::Config.doesnt_exist).to eql(nil)
+        expect(SlackMessaging::Config.doesnt_exist?).to eql(false)
       end
-
       it "should return the config value when set" do
-        SlackMessaging::Config.set_config_options(new_value: "testing")
+        SlackMessaging::Config.new_value = "testing"
         expect(SlackMessaging::Config.new_value).to eql("testing")
       end
     end
@@ -37,7 +18,7 @@ module SlackMessaging
     context "after loading a config file" do
       before do
         config_file = {"domain" => "example_domain",
-                       "slack" => {"slack_option"=>true,
+                       "slack" => {"slack_option" => true,
                                    "username" => "Rspec Tester",
                                    "icon_url" => "http://fake.url",
                                    "channel" => "#test-channel",
@@ -55,7 +36,7 @@ module SlackMessaging
 
       it "overwriting values should work" do
         expect(SlackMessaging::Config.slack).to be_kind_of(Hash)
-        SlackMessaging::Config.set_config_options(slack: "this is a string now")
+        SlackMessaging::Config.slack = "this is a string now"
         expect(SlackMessaging::Config.slack).to eql("this is a string now")
       end
     end
