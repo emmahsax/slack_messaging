ad7866da13ae43117c8bbebb12cdc42fcfa2147d
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-02 17:20:57 -0500

    Clean up require statements (#6)
    * Clean all of these up
    
    * I give up
    
    * Increase version
    
    * Here we go
    
    * Remove extra space
    
    * Woohoo this works!
    
    * Remove this gem

--- lib/slack_messaging/commands/slack.rb
+++
@@ -1,7 +0,0 @@
-desc 'Prints a variety of messages to Slack'
-command 'slack' do |c|
-  c.action do |global_options, options, args|
-    require_relative '../scripts/slack'
-    SlackMessaging::Scripts::Slack.execute(args, options) # args are optional string to print
-  end
-end
