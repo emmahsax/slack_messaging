7d3d0f9160285fee7eceb759c4805743ee66a0c9
Emma Sax <emma.sax4+git@gmail.com>
2020-11-18 22:41:36 -0600

    Update ruby style (#10)
    Update ruby style

--- lib/slack_messaging/slack.rb
+++ lib/slack_messaging/slack.rb
@@ -1,15 +1,11 @@
-require 'highline'
-
 module SlackMessaging
   class Slack
-    class << self
-      attr_accessor :options
-    end
+    attr_accessor :options
 
-    def self.execute(args, options=nil)
+    def self.execute(args, options = nil)
       if args.empty?
-        message = SlackMessaging::RandomMessage.new # generate a new random message
-        print_message(message.get_text)
+        message = SlackMessaging::RandomMessage.new
+        print_message(message.text)
       else
         args.each do |message|
           print_message(message)
@@ -18,8 +14,7 @@ module SlackMessaging
     end
 
     def self.print_message(message)
-      slack_job = SlackMessaging::NotifySlack.new(message)
-      slack_job.perform
+      SlackMessaging::NotifySlack.new(message).perform
     end
   end
 end
