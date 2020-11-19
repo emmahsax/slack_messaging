264a80a251abeff2ae456260cda3f09853cc4ae7
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-11-18 22:41:36 -0600

    Update ruby style (#10)
    Update ruby style

---
+++ lib/slack_messaging/default_paths.rb
@@ -0,0 +1,13 @@
+module SlackMessaging
+  class DefaultPaths
+    def self.config
+      File.join(self.home, '.slack_messaging.yml')
+    end
+
+    private
+
+    def self.home
+      ENV['HOME'] ? ENV['HOME'] : '.'
+    end
+  end
+end
