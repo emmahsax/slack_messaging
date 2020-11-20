67ea26f34d43a104a3118ed5e868dc1b99d294c7
Emma Sax <emma.sax4+git@gmail.com>
2020-11-20 08:13:19 -0600

    Use a random quotes API (#11)
    * Use the API
    
    * Update tests and documentation

--- lib/slack_messaging.rb
+++ lib/slack_messaging.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'hashie'
 require 'httparty'
 require 'highline'
+require 'json'
 
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), 'slack_messaging')) + '/**/*.rb'].each do |file|
   require_relative file
