87e2154fe3e2c98ca89bc4d1f5690fe7ce9c23d8
Emma Sax <emma.sax4+git@gmail.com>
2021-02-24 17:32:47 -0600

    Adding tests

--- spec/slack_messaging/config_spec.rb
+++ spec/slack_messaging/config_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 require 'slack_messaging'
 
 describe SlackMessaging::Config do
+  subject { SlackMessaging::Config }
+
   context 'config key methods' do
     it 'should return nil when not set' do
-      expect(SlackMessaging::Config.doesnt_exist).to eql(nil)
-      expect(SlackMessaging::Config.doesnt_exist?).to eql(false)
+      expect(subject.doesnt_exist).to eql(nil)
+      expect(subject.doesnt_exist?).to eql(false)
     end
 
     it 'should return the config value when set' do
       config_value = Faker::Lorem.word
-      SlackMessaging::Config.new_value = config_value
-      expect(SlackMessaging::Config.new_value).to eql(config_value)
+      subject.new_value = config_value
+      expect(subject.new_value).to eql(config_value)
     end
   end
 
@@ -35,19 +37,19 @@ describe SlackMessaging::Config do
     before do
       allow(YAML).to receive(:load_file).and_return(config_file)
       allow(File).to receive(:exist?).and_return(true)
-      SlackMessaging::Config.load(Faker::Lorem.word)
+      subject.load(Faker::Lorem.word)
     end
 
     it 'calling a method corresponding to a key in the file should return the value' do
-      expect(SlackMessaging::Config.domain).to eql(domain)
-      expect(SlackMessaging::Config.slack).to be_kind_of(Hash)
-      expect(SlackMessaging::Config.slack[:slack_option]).to eql(true)
+      expect(subject.domain).to eql(domain)
+      expect(subject.slack).to be_kind_of(Hash)
+      expect(subject.slack[:slack_option]).to eql(true)
     end
 
     it 'overwriting values should work' do
-      expect(SlackMessaging::Config.slack).to be_kind_of(Hash)
-      SlackMessaging::Config.slack = sentence
-      expect(SlackMessaging::Config.slack).to eql(sentence)
+      expect(subject.slack).to be_kind_of(Hash)
+      subject.slack = sentence
+      expect(subject.slack).to eql(sentence)
     end
   end
 end
